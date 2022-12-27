e8114903d322b10a159f7da6cafd480c5f205674
prajwal89 <22263436+prajwal89@users.noreply.github.com>
2022-12-27 15:32:41 +0530

    new examples

--- example.php
+++ example.php
@@ -3,11 +3,31 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 include 'vendor/autoload.php';
 
 use Prajwal89\ShareTo;
 
-$share_to = new ShareTo('title', 'url');
+echo '<hr><p>Get all available buttons</p> ';
+$share = new ShareTo('McqMate - MCQ Portal for Students', 'https://mcqmate.com/');
+echo $share->all()->getButtons();
 
-echo $share_to->all()->getButtons();
+
+echo '<hr><p>Get single button</p> ';
+$share2 = new ShareTo('McqMate - MCQ Portal for Students', 'https://mcqmate.com/');
+echo $share2->whatsapp()->getButtons();
+
+
+echo '<hr><p>Get multiple buttons</p> ';
+$share3 = new ShareTo('McqMate - MCQ Portal for Students', 'https://mcqmate.com/');
+echo $share3->whatsapp()->twitter()->getButtons();
+
+
+echo '<p style="text-align: center">OR</p>';
+$share4 = new ShareTo('McqMate - MCQ Portal for Students', 'https://mcqmate.com/');
+echo $share4->only(['whatsapp', 'twitter'])->getButtons();
+
+
+echo '<hr><p>Get all links</p> ';
+$share4 = new ShareTo('McqMate - MCQ Portal for Students', 'https://mcqmate.com/');
+$links = $share4->all()->getRawLinks();
+print("<pre>" . print_r($links, true) . "</pre>");
